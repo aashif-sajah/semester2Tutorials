bb471415d31f480b6b4205bc939d4d8943cb745e
Aashif <masaashifsajah@gmail.com>
2024-01-30 22:14:05 +0530

    MyFirstJava tutorial week 2

---
+++ MyFirstJava.java
@@ -0,0 +1,11 @@
+public class MyFirstJava{
+
+    public static void main(String[]args){
+            String name = "Aashif";
+            int age = 18;
+            System.out.println("My name is "+ name +" and I am "+ age + " old"); 
+
+
+    }
+
+}
