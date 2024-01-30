c36f10c97693411ab50ef293e4f08a1ba45416f0
Aashif <masaashifsajah@gmail.com>
2024-01-30 22:33:16 +0530

    Initial commit

---
+++ Ques2.java
@@ -0,0 +1,15 @@
+public class Ques2{
+    public static void main(String[]args){
+        int runningTotal = 0;
+
+        runningTotal = 5;
+        System.err.println("Variable value is: "+ runningTotal);
+        runningTotal = 8;
+        System.err.println("Variable value is: "+ runningTotal);
+        runningTotal = 13;
+        System.err.println("Variable value is: "+ runningTotal);
+        runningTotal = 15;
+        System.err.println("Variable value is: "+ runningTotal);
+
+    }
+}
