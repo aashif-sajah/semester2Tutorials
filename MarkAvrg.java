a8c0088fb17f4b1c81dfdefbe43771028d0f5b2d
Aashif <masaashifsajah@gmail.com>
2024-01-30 22:51:02 +0530

    Done and dusted the question three of week two tut

---
+++ MarkAvrg.java
@@ -0,0 +1,12 @@
+public class MarkAvrg {
+    public static void main(String[] args) {
+        double tamil = 75d;
+        double math = 75d;
+        double eng = 75d;
+
+        double avrg = 0;
+        avrg = (tamil+math+eng)/3;
+        System.out.println("The avarage mark is : "+avrg);
+
+    }
+}
